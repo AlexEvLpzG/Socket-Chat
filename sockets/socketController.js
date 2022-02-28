112fcc9916968092002fd30ea23be1c19cadc319
AlexiSkyline <ilegal_sprite@outlook.com>
2022-02-27 22:31:32 -0600

    Creamos el modelo de ChatMensajes

--- sockets/socketController.js
+++ sockets/socketController.js
@@ -1,14 +1,13 @@
 const { Socket } = require( 'socket.io' );
 const { comprobarJWT } = require( '../helpers' );
 
-const socketController = async( socket = new Socket() ) => {
+const socketController = async( socket = new Socket(), io ) => {
     const usuario = await comprobarJWT( socket.handshake.headers['x-token'] );
 
     if( !usuario ) {
         return socket.disconnect();
     }
 
-    console.log( 'Se conecto ', usuario.nombre );
 }
 
 module.exports = {
